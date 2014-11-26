d58d067325689fce3f1a825b2cc803b037d04964
Hankin Hwong <jinchaoh@163.com>
2014-11-27 00:06:25 +0800

    just for commit 2

--- howshouldido/urls.py
+++ howshouldido/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+from food.views import how_food
 from django.contrib import admin
 admin.autodiscover()
 
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'howshouldido.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^how_food/$', how_food),
     url(r'^admin/', include(admin.site.urls)),
+    url(),
 )
